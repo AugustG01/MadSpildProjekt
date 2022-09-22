2e28eacaafe62908087ae867bad1786eb641bce8
AugustG01 <augu8204@stud.kea.dk>
2022-09-22 12:38:14 +0200

    Initial commint

---
+++ src/main/java/com/example/madspildminiprojekt/MadSpildMiniProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.madspildminiprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadSpildMiniProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadSpildMiniProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/madspildminiprojekt/MadSpildMiniProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madspildminiprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadSpildMiniProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
